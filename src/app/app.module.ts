36eed976e583a8cce8a11ccaf3039a8451756ca4
Melissa <hibnermm@gmail.com>
2023-11-14 21:21:20 -0800

    WIP add bootstrap module and in index.html, as well as search and display placeholders in component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { TvShowComponent } from './tv-show/tv-show.component';
@@ -10,7 +11,8 @@ import { TvShowComponent } from './tv-show/tv-show.component';
     TvShowComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
