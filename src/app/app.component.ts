dc6e0f4398fe7e6b4fc41e40510c0ec9c0513549
TrieaZon <108979358+TrieaZon@users.noreply.github.com>
2023-12-10 01:43:57 -0800

    tweaks and merge ideas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tv-show-app2';
+
+  currentTvShow: ITvShow = {
+    title: '',
+    medium: '',
+    genres: '',
+    status:  '',
+    rating: 0,
+    summary: '',
+  }
+
+  constructor(private tvshowService: TvShowService){
+  }
+
+  ngOnInit(): void {
+    this.tvshowService.getCurrentTvShow('darling in the franxx')
+    .subscribe(data => this.currentTvShow = data);
+  } // this function initializes the home page with a completed search 
+
+  doSearch(searchValue: string){
+    const userInput = searchValue
+    this.tvshowService.getCurrentTvShow(userInput).subscribe(data => this.currentTvShow = data)
+  }
 }
